b4561f0cbaed2ec2e6590a251dd1e54c547b8c83
Rupsha <rupsha619@gmail.com>
2026-06-13 23:20:57 +0600

    task3

---
+++ server.js
@@ -0,0 +1,24 @@
+const express=require('express')
+const app=express()
+
+function gcd(a,b){
+ while(b)[a,b]=[b,a%b]
+ return a
+}
+
+app.get('/rupsha619_gmail_com',(req,res)=>{
+ let x=+req.query.x
+ let y=+req.query.y
+
+ if(
+  !Number.isInteger(x)||
+  !Number.isInteger(y)||
+  x<=0||y<=0
+ ){
+  return res.send('NaN')
+ }
+
+ res.send(String(x/gcd(x,y)*y))
+})
+
+app.listen(3000,()=>console.log('running'))
